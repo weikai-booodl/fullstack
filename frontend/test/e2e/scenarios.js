9e81667985918f5079fcd39db2b985237220b64e
weikai <weikai@booodl.com>
2014-06-02 14:37:14 +1000

    add login page e2e test

--- frontend/test/e2e/scenarios.js
+++ frontend/test/e2e/scenarios.js
@@ -14,30 +14,32 @@ describe('Fullstack app', function () {
     });
 
 
-//    describe('login page', function () {
-//
-//        it('should redirect to departement_info view when a manager logs in', function () {
-//            element(by.model('username')).sendKeys("Vishwani.Minakawa");
-//            element(by.model('password')).sendKeys("110039");
-//            element(by.css('#login_btn')).click();
-//            browser.getLocationAbsUrl().then(function (url) {
-//                expect(url.split('#')[1]).toBe('/deparment/d001/employees');
-//            });
-//        });
-//
-//
-//        it('should redirect to employee_info view when a non-manager logs in', function () {
-//            element(by.model('username')).sendKeys("Georgi.Facello");
-//            element(by.model('password')).sendKeys("10001");
-//            element(by.css('#login_btn')).click();
-//            browser.getLocationAbsUrl().then(function (url) {
-//                expect(url.split('#')[1]).toBe('/employees/10001');
-//            });
-//
-//
-//        });
-//
-//    });
+    describe('login page', function () {
+
+        it('should redirect to departement_info view when a manager logs in', function () {
+            browser.get('/#/login');
+            element(by.model('username')).sendKeys("Vishwani.Minakawa");
+            element(by.model('password')).sendKeys("110039");
+            element(by.css('#login_btn')).click();
+            browser.getLocationAbsUrl().then(function (url) {
+                expect(url.split('#')[1]).toBe('/departments/d001');
+            });
+        });
+
+
+        it('should redirect to employee_info view when a non-manager logs in', function () {
+            browser.get('/#/login');
+            element(by.model('username')).sendKeys("Georgi.Facello");
+            element(by.model('password')).sendKeys("10001");
+            element(by.css('#login_btn')).click();
+            browser.getLocationAbsUrl().then(function (url) {
+                expect(url.split('#')[1]).toBe('/employees/10001');
+            });
+
+
+        });
+
+    });
 
 
 });
